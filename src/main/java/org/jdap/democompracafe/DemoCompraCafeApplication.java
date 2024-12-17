ed46c03d1a8189a33a05d9394f2462cfe153e4ab
jose-arias <joseariaspantoj@gmail.com>
2024-12-17 17:06:29 -0500

    Arquitectura del proyecto 🚧

---
+++ src/main/java/org/jdap/democompracafe/DemoCompraCafeApplication.java
@@ -0,0 +1,13 @@
+package org.jdap.democompracafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCompraCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCompraCafeApplication.class, args);
+    }
+
+}
